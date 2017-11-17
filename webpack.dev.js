87769557100a08ebbe82d9faa4861a59692b227d
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-11-17 15:35:16 +0100

    stage for hybrid fixed

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,10 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
-const PLATFORM_ANDROID = './platforms/android/platform_www';
-const PLATFORM_IOS = './platforms/ios/platform_www';
+const PLATFORM_ANDROID = './platforms/android/assets/www';
+const PLATFORM_IOS = './platforms/ios/www';
 
  module.exports = function(env) {
   let contentBase = './dist';  
@@ -23,17 +24,23 @@ const PLATFORM_IOS = './platforms/ios/platform_www';
       host: '0.0.0.0' // needed to be served from your ip
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Test',
+        filename: 'index.html',
+        APP_ENV: env.APP_ENV,
+        template: './src/index.template.html',
+        minify: {
+          collapseWhitespace: false
+        }
+      }),
       // pass this in the global scope
       new webpack.DefinePlugin({
         __ENV__: JSON.stringify('development'),
         APP_ENV: JSON.stringify(env.APP_ENV),
-      }),
-
-/*  
-    disable in development
-    new ServiceWorkerWebpackPlugin({
+      }),    
+      new ServiceWorkerWebpackPlugin({
         entry: path.join(__dirname, 'src/sw.js'),
-      }) */
+      })
     ]
   });
  }
