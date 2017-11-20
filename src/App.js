d7e2155922282fb799b833ff7aae4aba33cebaf4
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-11-20 12:55:41 +0100

    add connection state "component"

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import './css/index.css';
 
 export default class App {
     constructor(message = 'default message'){
+        this.updateOnlineStatus = this.updateOnlineStatus.bind(this);
+        // title
         this.body = document.querySelector('body');
         this.h1Tag = document.createElement('h1');
         this.h1Tag.className = 'Title-message';   
         this.body.appendChild(this.h1Tag);
+
+        // loose connection
+        this.connectionState = document.createElement('div');
+        this.connectionState.className = 'Connection-state';
+        this.body.appendChild(this.connectionState);
+        this.state = {
+            online: navigator.onLine
+        }
+        this.attachEvents();        
         this.getInfo();
+    }
+
+    updateOnlineStatus(evt) {
+        this.state.online = navigator.onLine;
+        this.render();
+    }
+
+    attachEvents(){
+        window.addEventListener('online',  this.updateOnlineStatus);
+        window.addEventListener('offline', this.updateOnlineStatus);
     }
 
     async getInfo() {
@@ -24,5 +45,7 @@ export default class App {
 
     render() {
         this.h1Tag.innerText = this.message;
+        this.connectionState.className = ['Connection-state', this.state.online ? 'online' : 'offline' ].join(' ');
+        this.connectionState.innerHTML = `<div>Connection state:${this.state.online ? 'online' : 'offline'}</div>`;
     }
 }
