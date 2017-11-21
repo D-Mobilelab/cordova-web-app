25bcefbc61669a7c4eb01e8c8c10151076ab7789
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-11-21 09:52:26 +0100

    register service worker manually

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ function startApp() {
 
     if ('serviceWorker' in navigator) {
         if(__ENV__ !== 'development') { 
-            const registration = runtime.register();
+            const registration = navigator.serviceWorker.register('sw.js')
+                .then(function(reg) {
+                    // registration worked
+                    console.log('Registration succeeded. Scope is ' + reg.scope);
+                    return reg;
+                }).catch(function(error) {
+                    // registration failed
+                    console.log('Registration failed with ' + error);
+                });
         }
         // registration.unregister()<Promise>
     }
