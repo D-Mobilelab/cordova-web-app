57f28eee213997d1b2441336a969ef3bf3cb39a5
Aurelio Merenda <aurelio.merenda@docomodigital.com>
2018-01-10 17:21:40 +0100

    Final version of dynamic route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-
 const config = [{
     path: '/',
-    component: 'Home'
+    component: 'Home',
+    exact: true
 }, {
     path: '/zoom',
     component: 'Zoom'
@@ -14,26 +14,31 @@ export default class App extends Component {
     constructor() {
         super(...arguments);
         this.state = {
-            component: null
+            routes: []
         }
     }
     
     componentWillMount(){
         let routes = config.map((route, i) => {
             System.import('./pages/' + route.component).then(dynComponent => {
-                this.setState({ component: dynComponent.default })
+                this.setState((state, props) => { 
+                    route.component = dynComponent.default;
+                    return { routes: state.routes.concat(route) };
+                });
             });
         });
     }
 
     render() {
-        
+        let routesJSX = this.state.routes.map((route, i) => (
+            <Route key={i} {...route} />
+        ));
 
         return (
             <Router>
                 <div>
                     <p>header</p>
-                    {this.state.component ? <Route path='/' component={this.state.component} /> : null}
+                    {routesJSX}
                 </div>
             </Router>                
         );
