45ccbf485ce3f542c815e643232cfee821613c74
Aurelio Merenda <aurelio.merenda@docomodigital.com>
2018-01-10 17:00:33 +0100

    Removed onsenui; add first step of dynamic route

--- src/App.js
+++ src/App.js
@@ -1,60 +1,41 @@
 import React, { Component } from 'react';
-import { 
-    Page
-    , Toolbar
-    , ToolbarButton
-    , Icon
-    , Checkbox
-    , ListItem
-    , List
-    , ListHeader
-    , Button
-    , Range
-    , Carousel
-    , CarouselItem
-} from 'react-onsenui';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import * as api from './api/api.js';
+
+const config = [{
+    path: '/',
+    component: 'Home'
+}, {
+    path: '/zoom',
+    component: 'Zoom'
+}];
 
 export default class App extends Component {
     constructor() {
         super(...arguments);
-        this.renderRow = this.renderRow.bind(this);
         this.state = {
-            isFetching: false,
-            articles: []
+            component: null
         }
     }
     
-    async componentDidMount() {
-        this.setState({ isFetching: true });
-        const articles = await api.getArticles();
-        this.setState({ isFetching: false, articles });        
+    componentWillMount(){
+        let routes = config.map((route, i) => {
+            System.import('./pages/' + route.component).then(dynComponent => {
+                this.setState({ component: dynComponent.default })
+            });
+        });
     }
 
-    renderToolbar() {
-        return (
-            <Toolbar>
-                <div className='center'>Example app Test</div>
-            </Toolbar>
-        );
-    }
-
-    renderRow(row) {
-        return (<ListItem key={row} tappable>
-            {row.title}
-        </ListItem>);
-    }
-    
     render() {
+        
+
         return (
-            <Page renderToolbar={this.renderToolbar}>
-                <List
-                    dataSource={this.state.articles}
-                    renderHeader={() => <ListHeader>Articoli</ListHeader>}
-                    renderRow={this.renderRow}
-                />
-            </Page>       
+            <Router>
+                <div>
+                    <p>header</p>
+                    {this.state.component ? <Route path='/' component={this.state.component} /> : null}
+                </div>
+            </Router>                
         );
     }
 }
