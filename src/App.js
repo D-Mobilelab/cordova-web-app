4f1d45e711e90cf7f900e2c59ead48eb99336fcd
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2018-01-25 15:15:33 +0100

    simple routing

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const config = [{
-    path: '/',
-    component: 'Home',
-    exact: true
-}, {
-    path: '/zoom',
-    component: 'Zoom'
-}];
+import Home from "./pages/Home";
+import Zoom from "./pages/Zoom";
 
 export default class App extends Component {
-    constructor() {
-        super(...arguments);
-        this.state = {
-            routes: []
-        }
-    }
-    
-    componentWillMount(){
-        let routes = config.map((route, i) => {
-            System.import('./pages/' + route.component).then(dynComponent => {
-                this.setState((state, props) => { 
-                    route.component = dynComponent.default;
-                    return { routes: state.routes.concat(route) };
-                });
-            });
-        });
-    }
-
     render() {
-        let routesJSX = this.state.routes.map((route, i) => (
-            <Route key={i} {...route} />
-        ));
-
         return (
-            <Router>
-                <div>
-                    <p>header</p>
-                    {routesJSX}
-                </div>
-            </Router>                
+        <Router>
+            <div>
+                <Route exact path="/" component={Home}/>
+                <Route path="/zoom" component={Zoom}/>
+            </div>
+        </Router>         
         );
     }
 }
