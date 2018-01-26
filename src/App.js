bd380baaf0fb4f73fe5b5a1d876caafaa0720ec7
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2018-01-26 12:56:50 +0100

    simple routing with link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from "./pages/Home";
 import Zoom from "./pages/Zoom";
+
+const buttonNav = { 
+    padding: '5px', 
+    margin:'1px', 
+    background:'white', 
+    color: 'black' 
+};
 
 export default class App extends Component {
     render() {
         return (
         <Router>
             <div>
+                <nav style={{ display: 'inline-block' }}>
+                    <Link style={buttonNav} to="/">Home</Link>
+                    <Link style={buttonNav} to="/zoom">Zoom</Link>
+                </nav>
+            
                 <Route exact path="/" component={Home}/>
                 <Route path="/zoom" component={Zoom}/>
             </div>
